3e048caf71b9d9973f704d82d8c17707f7f130a1
mikekhoury135 <mikekhoury135@gmail.com>
2021-09-09 09:08:09 -0400

    Added Jquery Interaction
    Scroll to top arrow only on homepage so far.

---
+++ js/index.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+    $('.scrollToTop').click(function(){
+        $('html, body').animate({scrollTop : 0},100);
+        return false;        
+    });
+    $('.scroll-arrow').hover(function() {
+        $(this).animate({ margin: -10, width: "+=10", height: "+=10" });
+    }, function() {
+        $(this).animate({ margin: 0, width: "-=10", height: "-=10" });
+    });
+});
